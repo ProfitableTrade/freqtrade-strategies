58281667172979faa0d454a46ce474467b2d7658
UdaltsovYurii <153769320+UdaltsovYurii@users.noreply.github.com>
2023-12-26 19:21:14 +0000

    Update BreakEvenY.py

--- user_data/strategies/BreakEvenY.py
+++ user_data/strategies/BreakEvenY.py
@@ -1,37 +1,85 @@
 from freqtrade.strategy import IStrategy
+from freqtrade.vendor.qtpylib.indicators import crossed_above, crossed_below
 from pandas import DataFrame
-import talib.abstract as ta
 
-class ModifiedBreakEven(IStrategy):
+class Strategy00(IStrategy):
+    """
+    Strategy 00
+    author@: Yurii Udaltsov
+    github@: https://github.com/freqtrade/freqtrade-strategies
+
+    How to use it?
+    > python3 ./freqtrade/main.py -s Strategy00
+    """
+
     INTERFACE_VERSION: int = 3
 
-    # Встановлення ROI та стоп-лоссу з оригінальної стратегії BreakEven
-    minimal_roi = {
-        "0": 0.01,  # at least 1% at first
-        "10": 0     # after 10min, everything goes
-    }
-    stoploss = -0.01
+    # Оптимальний стоп-лосс або %max, розроблений для стратегії
+    stoploss = -0.03
 
-    # Встановлення таймфрейму
+    # Оптимальний таймфрейм для стратегії
     timeframe = '5m'
 
-    # Розрахунок індикаторів
+    # Trailing stoploss
+    trailing_stop = False
+    trailing_stop_positive = 0.5
+    trailing_stop_positive_offset = 0.6
+
+    # Запускати "populate_indicators" тільки для нової свічки
+    process_only_new_candles = True
+
+    # Експериментальні параметри
+    use_exit_signal = True
+    exit_profit_only = False
+
+    # Optional order type mapping
+    order_types = {
+        'entry': 'limit',
+        'exit': 'limit',
+        'stoploss': 'market',
+        'stoploss_on_exchange': False
+    }
+
+    def ema(self, dataframe: DataFrame, timeperiod: int) -> DataFrame:
+        """
+        Calculate EMA without using TA-Lib
+        """
+        k = 2 / (timeperiod + 1)
+        dataframe['ema'] = dataframe['close'].ewm(span=timeperiod, adjust=False).mean()
+        return dataframe['ema']
+
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe['ema20'] = ta.EMA(dataframe, timeperiod=20)
-        dataframe['ema30'] = ta.EMA(dataframe, timeperiod=30)
+        """
+        Adds EMA 15 and EMA 30 indicators to the given DataFrame
+        """
+        # Calculate and add EMA 15
+        dataframe['ema15'] = self.ema(dataframe, timeperiod=15)
+
+        # Calculate and add EMA 30
+        dataframe['ema30'] = self.ema(dataframe, timeperiod=30)
+
         return dataframe
 
-    # Логіка відкриття угод
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Generates buy signal based on EMA indicators
+        """
         dataframe.loc[
             (
-                (dataframe['ema20'] > dataframe['ema30']) &  # EMA 20 перетинає EMA 30 знизу вгору
-                (dataframe['ema20'].shift(1) <= dataframe['ema30'].shift(1))  # Попереднє значення EMA 20 було нижче або рівне EMA 30
+                crossed_above(dataframe['ema15'], dataframe['ema30'])
             ),
             'enter_long'] = 1
+
         return dataframe
 
-    # Логіка закриття угод (застосовується ROI та стоп-лосс)
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Тут немає спеціальної логіки для виходу, оскільки вона здійснюється через ROI та стоп-лосс
+        """
+        Generates sell signal based on EMA indicators
+        """
+        dataframe.loc[
+            (
+                crossed_below(dataframe['ema15'], dataframe['ema30'])
+            ),
+            'exit_long'] = 1
+
         return dataframe
