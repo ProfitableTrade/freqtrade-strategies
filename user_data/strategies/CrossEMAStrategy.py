3f121ad815b2d0eb8f7660796d665ddea6fd39f6
UdaltsovYurii <153769320+UdaltsovYurii@users.noreply.github.com>
2023-12-26 18:25:34 +0000

    Add CrossEMAStrategy

---
+++ user_data/strategies/CrossEMAStrategy.py
@@ -0,0 +1,37 @@
+from freqtrade.strategy.interface import IStrategy
+import talib.abstract as ta
+from pandas import DataFrame
+
+class CrossEMAStrategy(IStrategy):
+    INTERFACE_VERSION = 2
+    minimal_roi = {"0": 50}
+    stoploss = -0.05
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['ema_fast'] = ta.EMA(dataframe, timeperiod=28)
+        dataframe['ema_slow'] = ta.EMA(dataframe, timeperiod=48)
+        
+        stoch_rsi = ta.STOCHRSI(dataframe['close'], timeperiod=14, fastk_period=3, fastd_period=3)
+        dataframe['stoch_rsi_k'] = stoch_rsi[0]  # fastk
+        dataframe['stoch_rsi_d'] = stoch_rsi[1]  # fastd
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (dataframe['ema_fast'] > dataframe['ema_slow']) &
+                (dataframe['stoch_rsi_k'] < 0.20)
+            ),
+            'buy'] = 1
+
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (dataframe['ema_fast'] < dataframe['ema_slow']) &
+                (dataframe['stoch_rsi_k'] > 0.80)
+            ),
+            'sell'] = 1
+        return dataframe
