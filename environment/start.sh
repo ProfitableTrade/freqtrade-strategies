44f98ea8f514d8130fb3906d3ea442e4330cfdbf
dorofiykolya <dorofiy.kolya@gmail.com>
2024-01-07 00:42:52 +0000

    .acme.json

--- environment/start.sh
+++ environment/start.sh
@@ -6,5 +6,11 @@ source config.env
 # export
 echo DOMAIN=${DOMAIN} > .env
 
+# traefik
+ACME_FILE=.acme.json
+
+([[ -f "$ACME_FILE" ]] && echo "✅ $ACME_FILE file exist" || echo "{}" > "$ACME_FILE") || echo "❌ fail to make file $ACME_FILE"
+chmod 600 "$ACME_FILE" && echo "✅ $ACME_FILE set 600 permissions" || echo "❌ fail to set permissions $ACME_FILE"
+
 # start
 docker compose up -d
