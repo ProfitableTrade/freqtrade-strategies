18851e9c08d31d8b45d5be7e42d3850fd0df44d8
dorofiykolya <dorofiy.kolya@gmail.com>
2024-01-22 01:55:36 +0000

    trading

--- trade/trade.sh
+++ trade/trade.sh
@@ -12,28 +12,38 @@ export CONFIG_NAME=${CONFIG_NAME}
 export DRY_RUN_WALLET=${DRY_RUN_WALLET}
 export RUNTIME_TYPE=${RUNTIME_TYPE}
 
-if [[ "$RUNTIME_TYPE" == "dry-run" ]]; then
-  docker compose -f docker-compose-trade.yml \
-    run \
-    --detach \
-    --name ${ENV_NAME}-freqtrade \
-    --rm freqtrade trade \
-    --strategy ${STRATEGY} \
-    --config /freqtrade/user_data/${CONFIG_NAME}.json \
-    --dry-run \
-    --dry-run-wallet ${DRY_RUN_WALLET}
+function start_docker_compose {
+  if [[ "$RUNTIME_TYPE" == "dry-run" ]]; then
+    docker compose -f docker-compose-trade.yml \
+      run \
+      --detach \
+      --name ${ENV_NAME}-freqtrade \
+      --rm freqtrade trade \
+      --strategy ${STRATEGY} \
+      --config /freqtrade/user_data/${CONFIG_NAME}.json \
+      --dry-run \
+      --dry-run-wallet ${DRY_RUN_WALLET}
 
-elif [[ "$RUNTIME_TYPE" == "production" ]]; then
-  docker compose -f docker-compose-trade.yml \
-    run \
-    --detach \
-    --name ${ENV_NAME}-freqtrade \
-    --rm freqtrade trade \
-    --strategy ${STRATEGY} \
-    --config /freqtrade/user_data/${CONFIG_NAME}.json
+  elif [[ "$RUNTIME_TYPE" == "production" ]]; then
+    docker compose -f docker-compose-trade.yml \
+      run \
+      --detach \
+      --name ${ENV_NAME}-freqtrade \
+      --rm freqtrade trade \
+      --strategy ${STRATEGY} \
+      --config /freqtrade/user_data/${CONFIG_NAME}.json
 
-else
-  echo "RUNTIME_TYPE value is unknown: $RUNTIME_TYPE"
-  exit 1
+  else
+    echo "RUNTIME_TYPE value is unknown: $RUNTIME_TYPE"
+    exit 1
 
-fi
+  fi
+}
+
+start_docker_compose \
+  && sleep 5 \
+  && docker logs ${ENV_NAME}-freqtrade \
+  && sleep 5 \
+  && docker logs ${ENV_NAME}-freqtrade \
+  && sleep 5 \
+  && docker logs ${ENV_NAME}-freqtrade
