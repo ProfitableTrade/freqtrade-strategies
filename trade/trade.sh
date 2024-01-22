eb380cb9e0206d366f6b44d107d4e4a54ecfd91f
dorofiykolya <dorofiy.kolya@gmail.com>
2024-01-22 00:45:11 +0000

    start trade

---
+++ trade/trade.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e 
+
+source .env
+
+export DOMAIN=${DOMAIN}
+export TRADE_NAME=${TRADE_NAME}
+export ENV_NAME=${ENV_NAME}
+export STRATEGY=${STRATEGY}
+export CONFIG_NAME=${CONFIG_NAME}
+export DRY_RUN_WALLET=${DRY_RUN_WALLET}
+export RUNTIME_TYPE=${RUNTIME_TYPE}
+
+
+if [[ "$RUNTIME_TYPE" == "dry-run" ]]; then
+  docker compose -f docker-compose-trade.yml \
+    run \
+    --detach \
+    --name ${ENV_NAME}-freqtrade \
+    --rm freqtrade trade \
+    --strategy ${STRATEGY} \
+    --config /freqtrade/user_data/${CONFIG_NAME}.json \
+    --config /freqtrade/user_data/credentials.json \
+    --dry-run \
+    --dry-run-wallet ${DRY_RUN_WALLET} > output.txt 2> error.txt 
+
+elif [[ "$RUNTIME_TYPE" == "production" ]]; then
+  docker compose -f docker-compose-trade.yml \
+    run \
+    --detach \
+    --name ${ENV_NAME}-freqtrade \
+    --rm freqtrade trade \
+    --strategy ${STRATEGY} \
+    --config /freqtrade/user_data/${CONFIG_NAME}.json \
+    --config /freqtrade/user_data/credentials.json > output.txt 2> error.txt 
+
+else
+  echo "RUNTIME_TYPE value is unknown: $RUNTIME_TYPE"
+  exit 1
+
+fi
+
+
