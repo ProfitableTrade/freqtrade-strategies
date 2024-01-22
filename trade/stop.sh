14025dc50dfc46bd24eccddcbea8cf6e74888566
dorofiykolya <dorofiy.kolya@gmail.com>
2024-01-22 12:53:25 +0000

    fix stop.sh

--- trade/stop.sh
+++ trade/stop.sh
@@ -2,21 +2,29 @@
 
 set -e
 
-source ./trade.env
+if [[ ! -f ./trade.env ]]; then
 
-export DOMAIN=${DOMAIN}
-export TRADE_NAME=${TRADE_NAME}
-export ENV_NAME=${ENV_NAME}
-export STRATEGY=${STRATEGY}
-export CONFIG_NAME=${CONFIG_NAME}
-export DRY_RUN_WALLET=${DRY_RUN_WALLET}
-export RUNTIME_TYPE=${RUNTIME_TYPE}
+  echo "trade.env is not found"
 
-function stop_docker_compose {
-  docker compose -f docker-compose-trade.yml down
-}
+else
 
-stop_docker_compose \
-  && echo "--------------------------------------------------------------------" \
-  && docker logs ${ENV_NAME}-freqtrade \
-  && echo "--------------------------------------------------------------------"
+  source ./trade.env
+
+  export DOMAIN=${DOMAIN}
+  export TRADE_NAME=${TRADE_NAME}
+  export ENV_NAME=${ENV_NAME}
+  export STRATEGY=${STRATEGY}
+  export CONFIG_NAME=${CONFIG_NAME}
+  export DRY_RUN_WALLET=${DRY_RUN_WALLET}
+  export RUNTIME_TYPE=${RUNTIME_TYPE}
+
+  function stop_docker_compose {
+    docker compose -f docker-compose-trade.yml down
+  }
+
+  stop_docker_compose &&
+    echo "--------------------------------------------------------------------" &&
+    docker logs ${ENV_NAME}-freqtrade &&
+    echo "--------------------------------------------------------------------"
+
+fi
