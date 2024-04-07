319d91761337601eebf337b7f92813f8088d1c4c
Ihor Zamorskyi <ihorzam25@gmail.com>
2024-04-07 21:09:09 +0300

    Partial sell strategy

--- user_data/strategies/Strategy_SLpart_SOLUSDT.py
+++ user_data/strategies/Strategy_SLpart_SOLUSDT.py
@@ -1,5 +1,8 @@
 # --- Do not remove these libs ---
+import datetime
+from typing import Optional, Tuple, Union
 from freqtrade.strategy import IStrategy
+from freqtrade.persistence import Trade
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from pandas import DataFrame
@@ -16,18 +19,26 @@ class Strategy_SLpart_SOLUSDT(IStrategy):
     """
 
     INTERFACE_VERSION: int = 3
+    BIGGEST_PROFIT_KEY: str = "biggest_profit"
+    PL_SELL_HALF_KEY: str = "pl_sell_half"
+    PL_SELL_3_4_KEY: str = "pl_sell_3_4"
+    
+    position_adjustment_enable = True
 
     # Оптимальний стоп-лосс або %max, розроблений для стратегії
     stoploss = -0.05
+    
+    # TODO: Change for on start callback with ENV provision
+    pl = 0.05
 
     # Оптимальний таймфрейм для стратегії
     timeframe = '30m'
 
     # Налаштування трейлінг стоп-лосу
     trailing_stop = True  # Включення трейлінг стоп-лосу
-    trailing_stop_positive = 0.033  # Трейлінг стоп активується, коли прибуток досягає 3,3%
-    trailing_stop_positive_offset = 0.035  # Трейлінг стоп починає діяти, коли прибуток перевищує 3,5%
-    trailing_only_offset_is_reached = True  # Трейлінг стоп активується тільки після досягнення offset
+    trailing_stop_positive = 0.012  # Трейлінг стоп активується, коли прибуток досягає 3,3%
+    # trailing_stop_positive_offset = 0.035  # Трейлінг стоп починає діяти, коли прибуток перевищує 3,5%
+    # trailing_only_offset_is_reached = True  # Трейлінг стоп активується тільки після досягнення offset
 
     # запускати "populate_indicators" тільки для нової свічки
     process_only_new_candles = True
@@ -81,4 +92,41 @@ class Strategy_SLpart_SOLUSDT(IStrategy):
             'exit_long'] = 1
 
         return dataframe
+    
+    def adjust_trade_position(self, trade: Trade, current_time: datetime,
+                              current_rate: float, current_profit: float,
+                              min_stake: Optional[float], max_stake: float,
+                              current_entry_rate: float, current_exit_rate: float,
+                              current_entry_profit: float, current_exit_profit: float,
+                              **kwargs
+                              ) -> Union[Optional[float], Tuple[Optional[float], Optional[str]]]:
+        biggest_profit = trade.get_custom_data(self.BIGGEST_PROFIT_KEY, default=0)
+        half_closed = trade.get_custom_data(self.PL_SELL_HALF_KEY, default=False)
+        closed_3_4 = trade.get_all_custom_data(self.PL_SELL_3_4_KEY, default=False)
+        
+        if not biggest_profit:
+            biggest_profit = current_profit
+            trade.set_custom_data(self.BIGGEST_PROFIT_KEY, biggest_profit)
+            return None
+        else:
+            if current_profit > biggest_profit:
+                trade.set_custom_data(self.BIGGEST_PROFIT_KEY, current_profit)
+                trade.set_custom_data(self.PL_SELL_3_4_KEY, False)
+                trade.set_custom_data(self.PL_SELL_3_4_KEY, False)
+                return None
+            else:
+                profit_loss = 1 - current_profit / biggest_profit
+                if profit_loss > self.pl:
+                    return -trade.stake_amount
+                elif profit_loss > (self.pl / 2 + self.pl / 4) and not closed_3_4:
+                    trade.set_custom_data(self.PL_SELL_3_4_KEY, True)
+                    return - (trade.stake_amount / 2)
+                elif profit_loss > self.pl / 2 and not half_closed:
+                    trade.set_custom_data(self.PL_SELL_HALF_KEY, True)
+                    return - (trade.stake_amount / 2)
+                else:
+                    return None
+                    
+                    
+                
       
