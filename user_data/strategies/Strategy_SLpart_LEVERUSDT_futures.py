b2fe86a4dad372a638644cb1e34598fdceacc393
UdaltsovYurii <153769320+UdaltsovYurii@users.noreply.github.com>
2024-06-15 14:23:17 +0100

    Add new strategy for LEVER futures
    Change limit and market

---
+++ user_data/strategies/Strategy_SLpart_LEVERUSDT_futures.py
@@ -0,0 +1,213 @@
+# --- Do not remove these libs ---
+import datetime
+from typing import Optional, Tuple, Union
+from freqtrade.strategy import IStrategy
+from freqtrade.persistence import Trade
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+from freqtrade.strategy import stoploss_from_open
+from pandas import DataFrame
+import logging
+
+# --------------------------------
+
+class Strategy_SLpart_LEVERUSDT_futures(IStrategy):
+    """
+    Strategy_SLpart_LEVERUSDT_futures
+    author@: Yurii Udaltsov
+    github@: https://github.com/freqtrade/freqtrade-strategies
+
+    How to use it?
+    > python3 ./freqtrade/main.py -s Strategy_SLpart_LEVERUSDT_leverage
+    """
+
+    INTERFACE_VERSION: int = 3
+    BEST_PRICE_KEY: str = "best_price"
+    PL_SELL_HALF_KEY: str = "pl_sell_half"
+    PL_SELL_3_4_KEY: str = "pl_sell_3_4"
+    BE_ACTIVATED: str = "be_activated"
+    
+    position_adjustment_enable = True
+
+    # Оптимальний стоп-лосс або %max, розроблений для стратегії
+    stoploss = -0.06
+    
+    use_custom_stoploss = True
+    
+    can_short = True
+    
+    # TODO: Change for on start callback with ENV provision
+    pl = 0.036
+    
+    brakeeven = 0.012
+
+    # Оптимальний таймфрейм для стратегії
+    timeframe = '30m'
+
+    # запускати "populate_indicators" тільки для нової свічки
+    process_only_new_candles = True
+
+    # Експериментальні параметри (конфігурація має перевагу над ними, якщо встановлено)
+    use_exit_signal = True
+    exit_profit_only = False
+
+    # Optional order type mapping
+    order_types = {
+        'entry': 'limit',
+        'exit': 'market',
+        'stoploss': 'market',
+        "emergency_exit": "market",
+        "force_entry": "market",
+        "force_exit": "market",
+        'stoploss_on_exchange': True
+    }
+    
+    def bot_start(self, **kwargs) -> None:
+        self.logger = logging.getLogger(__name__)
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Adds EMA 20 and EMA 30 indicators to the given DataFrame
+        """
+        # Calculate and add EMA 20
+        dataframe['ema20'] = ta.EMA(dataframe, timeperiod=20)
+
+        # Calculate and add EMA 30
+        dataframe['ema30'] = ta.EMA(dataframe, timeperiod=30)
+
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Generates buy signal based on EMA indicators
+        A buy signal is generated when EMA 20 crosses above EMA 30
+        """
+        dataframe.loc[
+            (
+                qtpylib.crossed_above(dataframe['ema20'], dataframe['ema30'])
+            ),
+            'enter_long'] = 1
+        
+        dataframe.loc[
+            (
+                qtpylib.crossed_below(dataframe['ema20'], dataframe['ema30'])
+            ),
+            'enter_short'] = 1
+      
+        return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Generates sell signal based on EMA indicators
+        A sell signal is generated when EMA 20 crosses below EMA 30
+        """
+        dataframe.loc[
+            (
+                qtpylib.crossed_below(dataframe['ema20'], dataframe['ema30'])
+            ),
+            'exit_long'] = 1
+
+        dataframe.loc[
+            (
+                qtpylib.crossed_above(dataframe['ema20'], dataframe['ema30'])
+            ),
+            'exit_short'] = 1
+
+        return dataframe
+    
+    def leverage(self, pair: str, current_time: datetime, current_rate: float,
+                 proposed_leverage: float, max_leverage: float, entry_tag: Optional[str], side: str,
+                 **kwargs) -> float:
+        """
+        Налаштуйте кредитне плече для кожної нової угоди. Цей метод викликається лише у ф’ючерсному режимі.
+
+        :param pair: пара, яка зараз аналізується
+        :param current_time: об’єкт datetime, що містить поточну дату й час
+        :param current_rate: Ставка, розрахована на основі налаштувань ціни в exit_pricing.
+        :param proposed_leverage: кредитне плече, запропоноване ботом.
+        :param max_leverage: максимальне кредитне плече, дозволене для цієї пари
+        :param entry_tag: Необов’язковий entry_tag (buy_tag), якщо надається разом із сигналом покупки.
+        :param side: 'long' або 'short' - вказує напрямок запропонованої торгівлі
+        :return: Сума кредитного плеча, яка становить від 1,0 до max_leverage.
+        """
+        return 3.0
+    
+    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
+                        current_rate: float, current_profit: float, after_fill: bool,
+                        **kwargs) -> Optional[float]:
+        try:
+            be_activated = trade.get_custom_data(self.BE_ACTIVATED, default=False)
+            
+            current_price_rate = 0
+            if trade.is_short:
+                current_price_rate = trade.open_rate / current_rate - 1
+            else:
+                current_price_rate = current_rate / trade.open_rate - 1
+
+            if be_activated or current_price_rate > self.brakeeven:
+                if not be_activated: 
+                    trade.set_custom_data(self.BE_ACTIVATED, True)
+                    
+                return stoploss_from_open(0.003, current_profit, is_short=trade.is_short, leverage=trade.leverage)
+
+            return None
+        except Exception as e:
+            self.logger.info(f"Error occured during custom stoploss definition: {str(e)}")
+            return None
+    
+    def adjust_trade_position(self, trade: Trade, current_time: datetime,
+                              current_rate: float, current_profit: float,
+                              min_stake: Optional[float], max_stake: float,
+                              current_entry_rate: float, current_exit_rate: float,
+                              current_entry_profit: float, current_exit_profit: float,
+                              **kwargs) -> Union[Optional[float], Tuple[Optional[float], Optional[str]]]:
+        try:
+            best_price = trade.get_custom_data(self.BEST_PRICE_KEY, default=0)
+            half_closed = trade.get_custom_data(self.PL_SELL_HALF_KEY, default=False)
+            closed_3_4 = trade.get_custom_data(self.PL_SELL_3_4_KEY, default=False)
+            
+            if not best_price:
+                best_price = current_rate
+                trade.set_custom_data(self.BEST_PRICE_KEY, best_price)
+                return None
+            
+            profit_loss = 0
+                
+            if trade.is_short:
+                if current_rate < best_price:
+                    trade.set_custom_data(self.BEST_PRICE_KEY, current_rate)
+                    if half_closed:
+                        trade.set_custom_data(self.PL_SELL_HALF_KEY, False)
+                    if closed_3_4:
+                        trade.set_custom_data(self.PL_SELL_3_4_KEY, False)
+                    return None
+                else:
+                    profit_loss = 1 - best_price / current_rate     
+            else:
+                if current_rate > best_price:
+                    trade.set_custom_data(self.BEST_PRICE_KEY, current_rate)
+                    if half_closed:
+                        trade.set_custom_data(self.PL_SELL_HALF_KEY, False)
+                    if closed_3_4:
+                        trade.set_custom_data(self.PL_SELL_3_4_KEY, False)
+                    return None
+                else:
+                    profit_loss = 1 - current_rate / best_price
+            
+                    
+            if profit_loss > self.pl:
+                self.logger.info(f"Profit loss for trade {trade.id} is reached {profit_loss}. Sell all. Type: {trade.trade_direction}")
+                return - trade.stake_amount
+            elif not closed_3_4 and profit_loss > (self.pl / 2 + self.pl / 4):
+                self.logger.info(f"Profit loss for trade {trade.id} is reached {profit_loss}. Sell 3/4 of stake. Type: {trade.trade_direction}")
+                trade.set_custom_data(self.PL_SELL_3_4_KEY, True)
+                return - (trade.stake_amount / 2)
+            elif not half_closed and profit_loss > self.pl / 2:
+                self.logger.info(f"Profit loss for trade {trade.id} is reached {profit_loss}. Sell half of stake. Type: {trade.trade_direction}")
+                trade.set_custom_data(self.PL_SELL_HALF_KEY, True)
+                return - (trade.stake_amount / 2)
+            else:
+                return None
+        except Exception as e:
+            self.logger.info(f"Error occured during trade position adjustment: {str(e)}")
+            return None
